953099c39b7874b617ccaf770756c3fa2a6e3e71
Jarvis Mercer - (AMSL) <8873215+JARVIS-AI@users.noreply.github.com>
2024-11-16 02:22:49 +0330

    Update urls.py

--- subtitle_converter/urls.py
+++ subtitle_converter/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import upload_subtitle, counter
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', upload_subtitle, name='upload_subtitle'),
     path('addcount/', counter, name='counter'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
