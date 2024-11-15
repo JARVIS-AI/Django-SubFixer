51636ba1a585b981c6d363bd2b490a21ab10101a
Jarvis Mercer - (AMSL) <8873215+JARVIS-AI@users.noreply.github.com>
2024-11-15 17:58:21 +0330

    Create apps.py

---
+++ subtitle_converter/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SubtitleConverterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'subtitle_converter'
+    verbose_name = "subtitle_converter"
